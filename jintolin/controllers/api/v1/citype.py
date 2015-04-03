d5bdc3621eebec4e0cf61395ea12903167ffc023
Akira Yoshiyama <akirayoshiyama@gmail.com>
2015-04-03 22:53:12 +0900

    Add {POST/DELETE} /api/v1/citype/linkable/<ID1>/<ID2> controllers

--- jintolin/controllers/api/v1/citype.py
+++ jintolin/controllers/api/v1/citype.py
@@ -2,9 +2,34 @@
 #
 # (c)2015  Akira Yoshiyama <akirayoshiyama@gmail.com>
 
+from pecan.core import abort, request
+from pecan import expose
+from pecan.rest import RestController
+
+from jintolin import exception as exc
+from jintolin import model
 from jintolin.controllers.api.v1.base import BaseController
+
+
+class CiTypeLinkableController(RestController):
+
+    @expose('json')
+    def post(self, id, linkable_id):
+        try:
+            model.CITYPE.add_linkable(id, linkable_id)
+        except exc.LinkableError:
+            abort(400)
+
+    @expose('json')
+    def delete(self, id, linkable_id):
+        try:
+            model.CITYPE.delete_linkable(id, linkable_id)
+        except exc.LinkableError:
+            abort(400)
 
 
 class CiTypeController(BaseController):
 
     model_name = "CITYPE"
+
+    linkable = CiTypeLinkableController()
--- jintolin/tests/controller/v1/test_citype.py
+++ jintolin/tests/controller/v1/test_citype.py
@@ -6,6 +6,7 @@ class TestApiV1CitypeController(TestApiV1BaseController, FunctionalTest):
 
     model_name = "CITYPE"
     baseurl = "/api/v1/citype/"
+    linkableurl = "/api/v1/citype/linkable/%s/%s"
 
     sample1 = {
         "type": "object",
@@ -43,3 +44,23 @@ class TestApiV1CitypeController(TestApiV1BaseController, FunctionalTest):
         },
         "required": ["name"]
     }
+
+    def test_add_linkable(self):
+        response = self.app.post(
+            self.linkableurl % (self.id1, self.id2))
+        self.assertEqual(response.status_int, 200)
+
+        response = self.app.post(
+            self.linkableurl % (self.id1, self.id2), expect_errors=True)
+        self.assertEqual(response.status_int, 400)
+
+    def test_delete_linkable(self):
+        self.app.post(self.linkableurl % (self.id1, self.id2))
+
+        response = self.app.delete(
+            self.linkableurl % (self.id1, self.id2))
+        self.assertEqual(response.status_int, 200)
+
+        response = self.app.delete(
+            self.linkableurl % (self.id1, self.id2), expect_errors=True)
+        self.assertEqual(response.status_int, 400)
